213276a164df63610a5c0941217954ecb9ee1154
nosinska <natalia.osinska@nokia.com>
2024-12-22 16:53:58 +0100

    v1

--- DB.py
+++ DB.py
@@ -1,8 +1,8 @@
 import mysql.connector
 
 class Database:
-    def __init__(self, host, username, password):
-        self.my_db = mysql.connector.connect(host, username, password)
+    def __init__(self, host, port, username, password):
+        self.my_db = mysql.connector.connect(host=host, port=port, user=username, password=password)
 
     def create(self, file):
         self.cursor = self.my_db.cursor()
@@ -20,3 +20,7 @@ class Database:
 
     def last_hash(self, page_id):
         pass
+
+if __name__ == "__main__":
+    db = Database("127.0.0.1", 3306, "root", "root")
+    db.create("create_db.txt")
