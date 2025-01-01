adf4c7d0404cdf416d638618fd05c52ce8899285
OsinsqLew <106916621+OsinsqLew@users.noreply.github.com>
2025-01-01 19:21:45 +0100

    draft

--- DB.py
+++ DB.py
@@ -1,36 +1,116 @@
 import mysql.connector
+import requests
+import hashlib
+import logging
+from datetime import date, datetime
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+sh = logging.StreamHandler()
+logger.addHandler(sh)
+
+
+def get_page_hash(url):
+    page = requests.get("https://" + url)
+    page_binary = page.content
+    return hashlib.md5(page_binary).hexdigest().strip()
+
 
 class Database:
     def __init__(self, host, port, username, password):
         self.my_db = mysql.connector.connect(
-            host=host,
-            port=port,
-            user=username,
-            password=password,
-            database="ids"
-            )
+            host=host, port=port, user=username, password=password, database="ids"
+        )
 
     def create_tables(self, file):
-        self.cursor = self.my_db.cursor()
-        create_commands = ''.join(file.readlines()).split('\n\n')
+        cursor = self.my_db.cursor()
+        create_commands = "".join(file.readlines()).split("\n\n")
         for table in create_commands:
             try:
-                self.cursor.execute(table)
+                cursor.execute(table)
             except mysql.connector.errors.ProgrammingError:
                 continue
+        cursor.close()
 
-    def add_page():
-        pass
+    def add_snapshot(self, page_id, hash, page_path, changes_path):
+        cursor = self.my_db.cursor()
+        query_snapshots = f"INSERT INTO snapshots (website_id, day, hour, hash, page_body, changes) VALUES ('{page_id}', '{date.today().strftime("%d-%m-%Y")}', '{datetime.now().time()}', '{hash}', '{page_path}', '{changes_path}')"
+        try:
+            cursor.execute(query_snapshots)
+            logger.info(f"Snapshot for page {page_id} added successfully.")
+            self.my_db.commit()
+        except Exception as e:
+            logger.info(f"Error adding snapshot: {e}")
+            self.my_db.rollback()
+        finally:
+            cursor.close()
 
-    def update_last_hash(self, page_id):
-        pass
+    def add_page(self, url: str, page_path, changes_path):
+        cursor = self.my_db.cursor()
+        hash = get_page_hash(url)
+        query_websites = (
+            f"INSERT INTO websites (url, last_hash) VALUES ('{url}', '{hash}');"
+        )
+        try:
+            cursor.execute(query_websites)
+            page_id = cursor.lastrowid  # * gets the last inserted row's id
+            query_snapshots = f"INSERT INTO snapshots (website_id, day, hour, hash, page_body, changes) VALUES ('{page_id}', '{date.today().strftime("%d-%m-%Y")}', '{datetime.now().time()}', '{hash}', '{page_path}', '{changes_path}')"
+            cursor.execute(query_snapshots)
+            self.my_db.commit()
+            logger.info(f"Page {url} added successfully.")
+        except Exception as e:
+            logger.info(f"Error adding page: {e}")
+            self.my_db.rollback()
+        finally:
+            cursor.close()
 
-    def last_hash(self, page_id):
-        query = f"SELECT last_hash FROM ids WHERE id = {page_id};"
+    def get_urls(self):
+        cursor = self.my_db.cursor(dictionary=True)
+        query = "SELECT id, url FROM websites"
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            logger.info(f"Got {len(result)} urls.")
+        except Exception as e:
+            logger.info(f"Error getting urls: {e}")
+        finally:
+            cursor.close()
+        return result
 
+    def update_last_hash(self, page_id: int, hash: str):
+        cursor = self.my_db.cursor()
+        query = f"UPDATE websites SET last_hash = '{hash}' WHERE id = {page_id}"
+        try:
+            cursor.execute(query)
+            logger.info(f"Updated page: {page_id} hash.")
+            self.my_db.commit()
+        except Exception as e:
+            logger.info(f"Error getting last hash: {e}")
+            self.my_db.rollback()
+        finally:
+            cursor.close()
+
+    def last_hash(self, page_id: int):
+        cursor = self.my_db.cursor()
+        query = f"SELECT last_hash FROM websites WHERE id = {page_id};"
+        try:
+            cursor.execute(query)
+            result = cursor.fetchone()
+            cursor.close()
+            return result
+        except Exception as e:
+            logger.info(f"Error getting last hash: {e}")
+            cursor.close()
 
 
 if __name__ == "__main__":
     db = Database("localhost", 3306, "user", "user")
-    with open("create_db.txt") as create_file:
-        db.create_tables(create_file)
+    db.add_page("dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor.html", "123")
+    # urls = db._get_urls()
+    # print(urls)
+    # for url in urls:
+    #     print(url["id"])
+
+    # with open("create_db.txt") as create_file:
+    #     db.create_tables(create_file)
--- main.py
+++ main.py
@@ -1,37 +1,40 @@
-import requests
-import hashlib
 import DB
 import time
 import difflib
+import datetime
+import requests
 
-def get_page_hash(page):
-    page_binary = page.content
-    return hashlib.md5(page_binary).hexdigest()
 
-def get_changes(db, current_version):
+def get_changes(db, current_version, page_id: int, date_time: str):
     last_version = db.last_version()
-    changes = difflib.unified_diff(last_version.splitlines(), current_version.splitlines(), n=0)
-    changes_readable = [line for line in changes if not line.startswith(('---', '+++'))]
+    changes = difflib.unified_diff(
+        last_version.splitlines(), current_version.splitlines(), n=0
+    )
+    changes_readable = [line for line in changes if not line.startswith(("---", "+++"))]
     changes_readable = "\n".join(changes_readable)
-    with open("changes.txt", "w", encoding="utf-8") as changes_file:
+    with open(f"changes_{page_id}_{date_time}.txt", "w", encoding="utf-8") as changes_file:
         changes_file.write(changes_readable)
     return changes_readable, changes_file
 
 
 if __name__ == "__main__":
-    print(get_page_hash("https://wp.pl"))
-    urls = []
     db = DB.DataBase()
-    for url in urls:
-        page = requests.get(url)
-        while(True):
-            # sprawdzanie czy ten dzień jest już w tablicy
-            hash = get_page_hash()
-            if  hash == db.last_hash():
-                time.sleep(30)
+    websites = db._get_urls()
+    path_base = r'C:\Users\natalcia\Desktop\PWr\ZIT\intrusion_detection_system\backup'
+    while True:
+        for website in websites:
+            hash = DB.get_page_hash(website["url"])
+            if hash == db.last_hash():
+                time.sleep(60/len(websites))
+                continue
             else:
+                date_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+                page = requests.get("https://" + website['url'])
+                page = page.content
+                changes = get_changes(db, page.content, website['id'], datetime)
+                changes_path = path_base + f"\changes_{website['id']}_{date_time}.txt"
+                page_path = path_base + f"\page{website['id']}_{date_time}.txt"
+                with open(page_path, "w") as fout:
+                    fout.write(page)
                 db.update_last_hash(hash)
-
-
-
-
+                db.add_snapshot(page_id=website['id'], hash=hash, page_path=page_path, changes_path=changes_path)
